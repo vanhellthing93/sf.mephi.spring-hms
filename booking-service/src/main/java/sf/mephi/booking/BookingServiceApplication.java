bca1ecae7346daaef4e77aa3e6820194f643d612
VanHellthing <vanhellthing@mail.ru>
2026-01-02 14:46:51 +0300

    Create project structur

---
+++ booking-service/src/main/java/sf/mephi/booking/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package sf.mephi.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServiceApplication.class, args);
+    }
+
+}
---
+++ common-lib/src/main/java/sf/mephi/App.java
@@ -0,0 +1,13 @@
+package sf.mephi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
